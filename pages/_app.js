f946fc0966516d20c279914cd2cc7e867985bc38
Rômulo Evangelista Ferreira <romuloevangelista2011@gmail.com>
2021-01-26 17:40:25 -0300

    add background image

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,9 @@ import '../styles.css';
 export default function App() {
   return <div id="app">
     <div id="content">
-      <h1>@carolinieditor</h1>
+      <a href="https://www.instagram.com/carolinieditor/?hl=pt-br">
+        <h1>@carolinieditor</h1>
+      </a>
       <p>Novidades em breve...</p>  
     </div>
     <Footer />
